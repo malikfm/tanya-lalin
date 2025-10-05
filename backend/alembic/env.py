c0e540e13e8cb541d325891274ff47913c53c648
malikfm <mubarok.malikfajar@gmail.com>
2025-10-05 21:50:41 +0700

    restructure import, update log config, add additional migration params

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,25 +1,22 @@
 from alembic import context
-from app.core.config import settings
-from app.db.base import Base
-from logging.config import fileConfig
 from sqlalchemy import engine_from_config, pool
+
+from config import settings
+from app.db.base import Base
 # import all models before target_metadata is set
 from app.db import models  # noqa: register it to Base.metadata
+from logging_setup import setup_logger
 
+logger = setup_logger()
 config = context.config
 config.set_main_option(
     "sqlalchemy.url",
     f"postgresql+psycopg://{settings.db_user}:{settings.db_password}@{settings.db_host}:{settings.db_port}/{settings.db_name}",
 )
-
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
-
 target_metadata = Base.metadata
 
 
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode."""
     engine = engine_from_config(
         config.get_section(config.config_ini_section),
         prefix="sqlalchemy.",
@@ -30,6 +27,7 @@ def run_migrations_online() -> None:
         context.configure(
             connection=connection,
             target_metadata=target_metadata,
+            compare_type=True,
             compare_server_default=True
         )
 
@@ -38,14 +36,13 @@ def run_migrations_online() -> None:
 
 
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode."""
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
         target_metadata=target_metadata,
-        literal_binds=True,
-        dialect_opts={"paramstyle": "named"}
-
+        compare_type=True,
+        compare_server_default=True,
+        literal_binds=True
     )
 
     with context.begin_transaction():
@@ -56,4 +53,3 @@ if context.is_offline_mode():
     run_migrations_offline()
 else:
     run_migrations_online()
-
