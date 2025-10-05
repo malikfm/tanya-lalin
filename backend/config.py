b0cf3c758b7b3c2a1f5635bb13ab8d073f9da030
malikfm <mubarok.malikfajar@gmail.com>
2025-10-05 21:47:38 +0700

    add pydantic setting

---
+++ backend/config.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    log_level: str = "INFO"
+    db_host: str = "localhost"
+    db_port: int = 5432
+    db_user: str = "admin"
+    db_password: str = "admin"
+    db_name: str = "test_db"
+    db_schema: str = "test"
+    embedding_dim: int = 3072
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
